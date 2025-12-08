76121b53e050c1ccf985545621bd0572543376d9
Francis Pion <francispion@hotmail.com>
2025-12-08 17:03:48 -0500

    command_query

---
+++ lib/Logitar.CQRS/ICommandHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace Logitar.CQRS;
+
+/// <summary>
+/// Represents a handler for a specific command.
+/// </summary>
+/// <typeparam name="TCommand">The type of the command.</typeparam>
+/// <typeparam name="TResult">The type of the command result.</typeparam>
+public interface ICommandHandler<TCommand, TResult> where TCommand : ICommand<TResult>
+{
+  /// <summary>
+  /// Handles the specified command and returns its result.
+  /// </summary>
+  /// <param name="command">The command to handle.</param>
+  /// <param name="cancellationToken">The cancellation token.</param>
+  /// <returns>The command result.</returns>
+  Task<TResult> HandleAsync(TCommand command, CancellationToken cancellationToken = default);
+}
