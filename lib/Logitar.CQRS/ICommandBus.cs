c56e988376e28735b85ed8cb8256ed2676ed35be
Francis Pion <francispion@hotmail.com>
2025-12-08 19:26:30 -0500

    interfaces

---
+++ lib/Logitar.CQRS/ICommandBus.cs
@@ -0,0 +1,16 @@
+﻿namespace Logitar.CQRS;
+
+/// <summary>
+/// Represents a bus in which commands are sent.
+/// </summary>
+public interface ICommandBus
+{
+  /// <summary>
+  /// Executes the specified command.
+  /// </summary>
+  /// <typeparam name="TResult">The type of the command result.</typeparam>
+  /// <param name="command">The command to execute.</param>
+  /// <param name="cancellationToken">The cancellation token.</param>
+  /// <returns>The command result.</returns>
+  Task<TResult> ExecuteAsync<TResult>(ICommand<TResult> command, CancellationToken cancellationToken = default);
+}
